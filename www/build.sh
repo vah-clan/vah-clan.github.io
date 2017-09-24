3a38047cb79a924617f0ef61d767a0e8f26eaa8d
Dan <danbarry16@googlemail.com>
2017-09-25 07:50:41 +1300

    RSS feed build

--- www/build.sh
+++ www/build.sh
@@ -14,6 +14,7 @@ function main {
   case $1 in
     -b|--build)
       build
+      rss
       ;;
     -c|--clean)
       clean
@@ -41,6 +42,36 @@ function build {
       pandoc -s -c /www/style.css -B $(pwd)/header.html $z -o ${z%.*}.html
     done
   done
+}
+
+# rss()
+#
+# Build a statically generated RSS feed.
+function rss {
+  rm vah.rss
+  echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"   >> vah.rss
+  echo "<rss version=\"2.0\">"                         >> vah.rss
+  echo "  <channel>"                                   >> vah.rss
+  echo "    <title>|VAH| Clan</title>"                 >> vah.rss
+  echo "    <description>Blog updates</description>"   >> vah.rss
+  echo "    <link>https://vah-clan.github.io</link>"   >> vah.rss
+  echo "    <lastBuildDate>$(date -R)</lastBuildDate>" >> vah.rss
+  echo "    <pubDate>$(date -R)</pubDate>"             >> vah.rss
+  echo "    <ttl>1800</ttl>"                           >> vah.rss
+  for z in blg/*.md; do
+    title="$(sed '1q;d' $z)"
+    description="$(sed '5q;d' $z)"
+    link="https://vah-clan.github.io/www/blg/${z%.*}.html"
+    pubdate="$(date -r $z -R)"
+    echo "    <item>"                                    >> vah.rss
+    echo "      <title>$title</title>"                   >> vah.rss
+    echo "      <description>$description</description>" >> vah.rss
+    echo "      <link>$link</link>"                      >> vah.rss
+    echo "      <pubDate>$pubdate</pubDate>"             >> vah.rss
+    echo "    </item>"                                   >> vah.rss
+  done
+  echo "  </channel>"                                  >> vah.rss
+  echo "</rss>"                                        >> vah.rss
 }
 
 # clean()
